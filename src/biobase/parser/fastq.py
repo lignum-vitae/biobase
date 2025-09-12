2e7192ece731b2566b473cd7b5333071634b0085
Andrew Hennis <dawnandrew100@gmail.com>
2025-09-11 21:59:58 -0400

    unifying fasta and fastq scripts

--- src/biobase/parser/fastq.py
+++ src/biobase/parser/fastq.py
@@ -29,7 +29,7 @@ def main() -> None:
     CGGTGTACTTCGTTCCAGCTAGATTTGGGTGCATGACCATACCGTGACAAGAAAGTTGTCGGTATCTTTGTGTTTCTGTTGGTGCTGATATTGCCGCACCGCCCGTCGCTACACCGATTGTTCTGTTGGTCTTGAAACACGGACCAGGGTCTAGAGCAG
     +
     %$&'$&'&&'0,42%*$&&%$%$#$)$*+,'($&))(*$%$%'-8644(()-&'%&*'')%*('579:?.*,9:+)1-9.'(7491:7,(52.11'7;:<===E@;>448,,(%*.''*,%&$-.;<:;66138/**,2?8<:**'%&)%&#$&&,,'&"""
-    fastq : FastqParser = FastqParser(fastq_seq)
+    fastq: FastqParser = FastqParser(fastq_seq)
     print(f"Total reads: {fastq.count_reads()}")
     for read in fastq:
         print(read)
@@ -38,7 +38,7 @@ def main() -> None:
 
 
 class FastqRecord:
-    def __init__(self, id: str, seq: str, separator:str, quality:str) -> None:
+    def __init__(self, id: str, seq: str, separator: str, quality: str) -> None:
         # Validation is done at the file level
         self.id = id
         self.seq = seq
@@ -47,7 +47,11 @@ class FastqRecord:
 
     # Behavior in case of print or repr
     def __repr__(self) -> str:
-        return f"<FastqRecord {self.id} len={len(self.seq)}"
+        return f"FastqRecord(id={self.id!r}, seq_len={len(self.seq)})"
+
+    def __str__(self) -> str:
+        preview = self.seq[:20] + ("..." if len(self.seq) > 20 else "")
+        return f"{self.id} | {preview}"
 
     # FastqRecord level utility
     def length(self) -> int:
@@ -57,63 +61,72 @@ class FastqRecord:
         return f">{self.id}\n{self.seq}"
 
     def phred_scores(self) -> np.ndarray:
-        return np.fromiter((ord(ch) - 33 for ch in  self.quality), dtype=np.int16)
+        return np.fromiter((ord(ch) - 33 for ch in self.quality), dtype=np.int16)
 
     def average_quality(self) -> float:
-        scores : np.ndarray = self.phred_scores()
+        scores: np.ndarray = self.phred_scores()
         return sum(scores) / scores.size if scores.size > 0 else 0.0
 
 
-class FastqFile:
-    def __init__(self, filepath:str) -> None:
+class FastqFileParser:
+    def __init__(self, filepath: str) -> None:
         self.filepath = filepath
 
     def __iter__(self) -> Iterator[FastqRecord]:
-        with open(self.filepath, 'r') as f:
+        with open(self.filepath, "r") as f:
             for line_number, line in enumerate(f, start=1):
                 line = line.strip()
                 # Keep track on where we are on the file
                 pos = line_number % 4
 
                 # Just in case there are no empty lines before the first line
-                if pos == 1: 
-                    if not line.startswith('@'):
-                        raise ValueError(f"Invalid Fastq format Expecetd '@' at line {line_number} got {line!r}")
-                    read_identifier = line.lstrip('@')
+                if pos == 1:
+                    if not line.startswith("@"):
+                        raise ValueError(
+                            f"Invalid Fastq format Expecetd '@' at line {line_number} got {line!r}"
+                        )
+                    read_identifier = line.lstrip("@")
                 elif pos == 2:
                     nucleotide_sequence = line.strip()
                     nucleotide_sequence = Dna._validate_dna_sequence(line)
                 # Again to make sure that there are no empty lines in between
-                elif pos == 3: 
-                    if not line.startswith('+'):
-                        raise ValueError(f"Invalid Fastq format Expecetd '+' at line {line_number} got {line!r}")
+                elif pos == 3:
+                    if not line.startswith("+"):
+                        raise ValueError(
+                            f"Invalid Fastq format Expecetd '+' at line {line_number} got {line!r}"
+                        )
                     separator = line.strip()
                 elif pos == 0:
                     read_quality = line.strip()
-                    yield FastqRecord(read_identifier, nucleotide_sequence, separator, read_quality)
+                    yield FastqRecord(
+                        read_identifier, nucleotide_sequence, separator, read_quality
+                    )
 
     # Operations on all reads
     def count_reads(self) -> int:
         return sum(1 for _ in self)
+
     def filter_reads(self, min_avg_quality: float) -> Iterator[FastqRecord]:
-        for read in self :
+        for read in self:
             if read.average_quality() >= min_avg_quality:
                 yield read
+
     def to_fasta(self, out_path: str) -> None:
-        with open(out_path, 'w') as  o:
+        with open(out_path, "w") as o:
             for read in self:
-                o.write(read.convert_to_fasta() + '\n')
+                o.write(read.convert_to_fasta() + "\n")
+
     def read_lengths(self) -> np.ndarray:
-        lengths : list[int] = []
+        lengths: list[int] = []
         for read in self:
             lengths.append(read.length())
         # Using numpy arrays as the return because it will be more efficient for the further analysis
-        lengths_array : np.ndarray = np.array(lengths)
+        lengths_array: np.ndarray = np.array(lengths)
         return lengths_array
 
 
 class FastqParser:
-    def __init__(self, reads:str) -> None:
+    def __init__(self, reads: str) -> None:
         self.reads = reads
 
     def __iter__(self) -> Iterator[FastqRecord]:
@@ -123,40 +136,58 @@ class FastqParser:
             pos = line_number % 4
 
             # Just in case there are no empty lines before the first line
-            if pos == 1: 
-                if not line.startswith('@'):
-                    raise ValueError(f"Invalid Fastq format Expecetd '@' at line {line_number} got {line!r}")
-                read_identifier = line.lstrip('@')
+            if pos == 1:
+                if not line.startswith("@"):
+                    raise ValueError(
+                        f"Invalid Fastq format Expecetd '@' at line {line_number} got {line!r}"
+                    )
+                read_identifier = line.lstrip("@")
             elif pos == 2:
                 nucleotide_sequence = line.strip()
                 nucleotide_sequence = Dna._validate_dna_sequence(line)
             # Again to make sure that there are no empty lines in between
-            elif pos == 3: 
-                if not line.startswith('+'):
-                    raise ValueError(f"Invalid Fastq format Expecetd '+' at line {line_number} got {line!r}")
+            elif pos == 3:
+                if not line.startswith("+"):
+                    raise ValueError(
+                        f"Invalid Fastq format Expecetd '+' at line {line_number} got {line!r}"
+                    )
                 separator = line.strip()
             elif pos == 0:
                 read_quality = line.strip()
-                yield FastqRecord(read_identifier, nucleotide_sequence, separator, read_quality)
+                yield FastqRecord(
+                    read_identifier, nucleotide_sequence, separator, read_quality
+                )
 
     # Operations on all reads
     def count_reads(self) -> int:
         return sum(1 for _ in self)
+
     def filter_reads(self, min_avg_quality: float) -> Iterator[FastqRecord]:
-        for read in self :
+        for read in self:
             if read.average_quality() >= min_avg_quality:
                 yield read
+
     def to_fasta(self, out_path: str) -> None:
-        with open(out_path, 'w') as  o:
+        with open(out_path, "w") as o:
             for read in self:
-                o.write(read.convert_to_fasta() + '\n')
+                o.write(read.convert_to_fasta() + "\n")
+
     def read_lengths(self) -> np.ndarray:
-        lengths : list[int] = []
+        lengths: list[int] = []
         for read in self:
             lengths.append(read.length())
         # Using numpy arrays as the return because it will be more efficient for the further analysis
-        lengths_array : np.ndarray = np.array(lengths)
+        lengths_array: np.ndarray = np.array(lengths)
         return lengths_array
+
+
+def fastq_parser(fastq: str) -> list[FastqRecord]:
+    return list(FastqParser(fastq))
+
+
+def fastq_file_parser(file_path: str) -> list[FastqRecord]:
+    return list(FastqFileParser(file_path))
+
 
 if __name__ == "__main__":
     main()
--- tests/test_fastq.py
+++ tests/test_fastq.py
@@ -1,7 +1,13 @@
 import numpy as np
 import pytest
 
-from biobase.parser import FastqFile, FastqParser, FastqRecord
+from biobase.parser import (
+    FastqFileParser,
+    FastqParser,
+    FastqRecord,
+    fastq_parser,
+    fastq_file_parser,
+)
 
 SAMPLE_FASTQ = """@2fa9ee19-5c51-4281-abdd-eac8663f9b49 runid=f53ee40429765e7817081d4bcdee6c1199c2f91d sampleid=18S_amplicon read=109831 ch=33 start_time=2019-09-12T12:05:03Z
 CGGTAGCCAGCTGCGTTCAGTATGGAAGATTTGATTTGTTTAGCGATCGCCATACTACCGTGACAAGAAAGTTGTCAGTCTTTGTGACTTGCCTGTCGCTCTATCTTCCAGACTCCTTGGTCCGTGTTCAATCCCGGTAGTAGCGACGGGCGGTGTATGTATTATCAGCGCAACAGAAACAAAGACACC
@@ -13,7 +19,8 @@ GATGCATACTTCGTTCGATTTCGTTTCAACTGGACAACCTACCGTGACAAAGAAAGTTGTCGATGCTTTGTGACTTGCTG
 &%&%''&'+,005<./'%*-)%(#$'$#$%&&'('$$..74483=.0412$*/,)9/194/+('%%(+1+'')+,-&,>;%%.*@@D>>?)3%%296070717%%16;<'<236800%(,734(0$7769879@;?8)09:+/4'1+**7<<4.4,%%(.)##%&'(&&%*++'&#%$
 """
 
-def test_fastq_parser_multi_record():
+
+def test_fastq_parser_class_multi_record():
     records = list(FastqParser(SAMPLE_FASTQ))
     assert len(records) == 2
     # Record 1
@@ -42,6 +49,19 @@ def test_fastq_parser_multi_record():
     assert scores2.min() >= 0
     assert scores2.max() < 100
 
+
+def test_fastq_parser_multi_record():
+    records = fastq_parser(SAMPLE_FASTQ)
+    assert len(records) == 2
+    # Record 1
+    r1 = records[0]
+    assert r1.id.startswith("2fa9ee19-5c51")
+    assert "runid=" in r1.id
+    assert r1.seq.startswith("CGGTAGCCAGCTGCG")
+    assert len(r1.seq) == len(r1.quality)  # always true for FASTQ
+    assert r1.length() == len(r1.seq)
+
+
 def test_fastq_record_and_repr():
     # repr and print have the same behavior for the FastqParser - only repr is defined in the class
     # They include the id of the read (first line without @) and the read seq length
@@ -49,7 +69,8 @@ def test_fastq_record_and_repr():
     s = str(record)
     r = repr(record)
     assert record.id in s, r
-    assert str(record.length()) in s, r
+    assert str(record.length()) in r
+
 
 def test_fastq_parser_single_record():
     single_fastq = """@2fa9ee19-5c51-4281-abdd-eac86
@@ -58,13 +79,15 @@ CGGTAGCCAGCTGCGTTCAGTATG
 %%%+++'''@@@???<<<??????"""
     records = list(FastqParser(single_fastq))
     assert len(records) == 1
-    r : FastqRecord = records[0]
+    r: FastqRecord = records[0]
     assert r.id == "2fa9ee19-5c51-4281-abdd-eac86"
     assert r.seq == "CGGTAGCCAGCTGCGTTCAGTATG"
-def test_fastq_file_parser(tmp_path):
+
+
+def test_fastq_file_parser_class(tmp_path):
     fastq_file = tmp_path / "test.fastq"
     fastq_file.write_text(SAMPLE_FASTQ)
-    parser = FastqFile(str(fastq_file)) # Produce a read object and can be listed
+    parser = FastqFileParser(str(fastq_file))  # Produce a read object and can be listed
     records = list(parser)
     assert len(records) == 2
     # Record 1
@@ -78,6 +101,26 @@ def test_fastq_file_parser(tmp_path):
     assert len(r2.seq) == len(r2.quality)
     assert r2.length() == len(r2.seq)
 
+
+def test_fastq_file_parser(tmp_path):
+    fastq_file = tmp_path / "test.fastq"
+    fastq_file.write_text(SAMPLE_FASTQ)
+    records = fastq_file_parser(
+        str(fastq_file)
+    )  # Produce a read object and can be listed
+    assert len(records) == 2
+    # Record 1
+    r1 = records[0]
+    assert r1.id.startswith("2fa9ee19-5c51")
+    assert "runid=" in r1.id
+    # Record 2
+    r2 = records[1]
+    assert r2.id.startswith("1f9ca490-2f25")
+    assert r2.seq.startswith("GATGCATACTTCGTT")
+    assert len(r2.seq) == len(r2.quality)
+    assert r2.length() == len(r2.seq)
+
+
 def test_to_fasta(tmp_path):
     out_path = tmp_path / "out.fasta"
     # Get the first record
@@ -90,6 +133,7 @@ def test_to_fasta(tmp_path):
 
     assert "CGGTA" in fasta_content
     assert "GATGC" in fasta_content
+
 
 def test_invalid_fastq_header_raises_value_eror():
     bad_fasta = """This is a bad fastq"""
